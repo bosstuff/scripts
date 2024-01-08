c1510aebc473a9f4f93b1cf2c7c45477b336b99f
mxcsan <mrxcastro@gmail.com>
2024-01-08 11:09:18 -0300

    Delete hardware-hash-from-end-autopilot.ps1

--- hardware-hash-from-end-autopilot.ps1
+++
@@ -1,5 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-PowerShell.exe -ExecutionPolicy Unrestricted
-Install-Script -name Get-WindowsAutopilotInfo -Force
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
-Get-WindowsAutopilotInfo -Online
