fbcafac8d9313a701aa86d39ff7890e54e45b16a
mxcsan <mrxcastro@gmail.com>
2024-01-08 10:29:49 -0300

    Update exec policy hardware-hash-from-end-autopilot.ps1

--- hardware-hash-from-end-autopilot.ps1
+++ hardware-hash-from-end-autopilot.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-PowerShell.exe -ExecutionPolicy Bypass
+PowerShell.exe -ExecutionPolicy Unrestricted
 Install-Script -name Get-WindowsAutopilotInfo -Force
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Get-WindowsAutopilotInfo -Online
